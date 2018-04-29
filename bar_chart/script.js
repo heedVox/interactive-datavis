2f305563ba4897853704ef2f8a929d5c5aadcd71
Johnny Guye <johnnyq@hotmail.fr>
2018-04-30 02:16:39 +0300

    Fix width, changed value notation

--- bar_chart/script.js
+++ bar_chart/script.js
@@ -2,7 +2,7 @@ let showTsv = ( filename ) => {
 
   var margin = {top: 40, right: 20, bottom: 30, left: 40},
       width = 480 - margin.left - margin.right,
-      height = 250 - margin.top - margin.bottom;
+      height = 150 - margin.top - margin.bottom;
 
   var formatQuantitative = d3.format("");
 
@@ -10,7 +10,7 @@ let showTsv = ( filename ) => {
       .range([0, width]);
 
   var y = d3.scale.ordinal()
-      .rangeRoundBands([0, height], .1);
+      .rangeRoundBands([0, height], .3);
 
 
   var xAxis = d3.svg.axis()
@@ -27,7 +27,7 @@ let showTsv = ( filename ) => {
     .attr('class', 'd3-tip')
     .offset([-10, 0])
     .html(function(d) {
-      return "<strong>Value:</strong> <span style='color:red'>" + d.value + "</span>";
+      return `<strong style='color:#C0C0F0'>${scientificNotation(d.value)} Hz</strong>`;
     })
 
   var container = d3.select("body").append("div")
@@ -49,6 +49,12 @@ let showTsv = ( filename ) => {
       .attr("class", "y axis")
       .attr("transform", `translate(0,${height})`)
       .call(xAxis)
+      .append("text")
+        .attr("x", width/2)
+        .attr("y", 20)
+        .attr("dy", ".71em")
+        .style("text-anchor", "middle")
+        .text("Mean frequency (Hz)");
     // svg.append("g")
     //     .attr("class", "x axis")
     //     .attr("transform", "translate(0," + height + ")")
@@ -57,19 +63,13 @@ let showTsv = ( filename ) => {
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis)
-      .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", -15)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Mean frequency (Hz)");
 
     svg.selectAll(".bar")
         .data(data)
       .enter().append("rect")
         .attr("class", "bar")
         .attr("x", 0)
-        .attr("width", function(d) { return width - x(d.value); })
+        .attr("width", function(d) { return x(d.value); })
         .attr("y", function(d) { return y(d.dataType); })
         .attr("height", y.rangeBand())
         .on('mouseover', tip.show)
@@ -87,6 +87,14 @@ function type(d) {
   d.dataType = d.dataType.replace("Meanfreq", "")
   d.value = +d.value;
   return d;
+}
+
+function scientificNotation(val, pres) {
+  let p = Math.ceil(Math.log10(val))
+  val *= Math.pow(10,-p+1)
+  val = val.toFixed(pres | 3)
+  if(p != 1) return val + ` *10^${p-1}`
+  else  return val
 }
 
 for (let i = 1; i <= 20; i++) {
